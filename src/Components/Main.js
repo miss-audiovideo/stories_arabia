e4eabffe453511947106ce739968538cb65a829c
tejasksaindane <72094501+tejasksaindane@users.noreply.github.com>
2021-11-28 21:28:13 +0530

    video change

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,19 +1,18 @@
 import React from "react";
-import video from "./Videos/Shoes Ad.MOV";
-
+// import video from "./Videos/Shoes Ad.MOV";
+import Videoone from "./Videos/residence.mp4";
 const Main = () => {
   return (
     <>
       <video muted autoPlay loop className="video-container">
-        <source src={video} type="video/mp4" />
+        <source src={Videoone} type="video/mp4" />
       </video>
       <div className="heading">
         <h1 className="animate__animated animate__fadeInUp animate__delay-2s">
           Stories Arabia
         </h1>
         <h3 className="animate__animated animate__fadeInUp animate__delay-4s">
-        A one-stop site for all your digital marketing needs. 
-
+          A one-stop site for all your digital marketing needs.
         </h3>
       </div>
       <div className="video-overlay"></div>
